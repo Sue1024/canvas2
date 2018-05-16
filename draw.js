0cf804fc907c45013259d218308667bdadea4644
Xianshu Zhao <1589893069@qq.com>
2018-05-16 22:22:53 +0800

    first commit

---
+++ draw.js
@@ -0,0 +1,140 @@
+(function() {
+  var defaultOptions = {
+      legend: {
+          position: 'right',
+          font: {
+            weight: 'bold',
+            size: 20,
+            family: 'Arial'
+          }
+      },
+      title: {
+          text: 'Pie Chart',
+          font: {
+            weight: 'bold',
+            size: 20,
+            family: 'Arial'
+          }
+      },
+      tooltip: {
+          template: '<div>Year: {{label}}</div><div>Production: {{data}}</div>',
+          font: {
+            weight: 'bold',
+            size: 20,
+            family: 'Arial'
+          }
+      }
+  };
+
+  var plots = [];
+  var ctx, width, height, op, title_text, title_position, title_height,
+      title_width, radius, center, legend_width, legend_height,
+      legend_posX, legend_posY, legend_textX, legend_textY, startAngle = 0, endAngle = 0;
+
+  this.drawCircle = function(data, canvas, options){
+      canvas.onmousemove = onMouseMove;
+      if(canvas.getContext) {
+        ctx = canvas.getContext("2d");
+        width = canvas.width;
+        height = canvas.height;
+
+        op = generateOptions(options, defaultOptions);
+        title_text = op.title.text;
+
+        ctx.font = op.title.font.weight + " " + op.title.font.size+"px " + op.title.font.family;
+        title_width = ctx.measureText(title_text).width;
+        title_height = op.title.font.size;
+
+        title_position = {
+          x: (width , title_width)/2,
+          y: 20 + title_height
+        };
+
+        ctx.fillText(title_text, title_position.x, title_position.y);
+
+        radius = (height - title_height - title_position.y - 20) / 2 ;
+        center = {
+          x: radius + 20,
+          y: radius + 30 + title_position.y
+        };
+        legend_width = op.legend.font.size * 2.5;
+        legend_height = op.legend.font.size * 1.2;
+        legend_posX = center.x * 2 +20;
+        legend_posY = 80;
+        legend_textX = legend_posX + legend_width + 5;
+        legend_textY = legend_posY + op.legend.font.size * 0.9;
+        ctx.strokeStyle = 'grey';
+        ctx.lineWidth = 3;
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+        var data_c = calculateData(data);
+        for(var i=0, len=data_c.length; i<len; i++) {
+            endAngle += data_c[i].portion * 2*Math.PI;
+            var plot = {
+              start: startAngle,
+              end: endAngle,
+              color: data_c[i].color
+            };
+            plots.push(plot);
+            drawPlot(data_c[i].color)
+            startAngle = endAngle;
+            legend_posY += (10 + legend_height);
+            legend_textY += (10 + legend_height);
+            drawLegend(data_c[i].label, data_c[i].portion);
+        }
+
+      }
+  };
+  function drawPlot(color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(center.x, center.y);
+    ctx.arc(center.x, center.y, radius, startAngle, endAngle, false);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function drawLegend(label, portion) {
+    ctx.fillRect(legend_posX, legend_posY, legend_width, legend_height);
+    ctx.font = 'bold 12px Arial';
+    var percent = label + ' : ' + (portion * 100).toFixed(2) + '%';
+    ctx.fillText(percent, legend_textX, legend_textY);
+  }
+
+  function onMouseMove(e) {
+
+  }
+
+  function mergeJSON(source1,source2){
+    var mergedJSON = JSON.parse(JSON.stringify(source2));
+    for (var attrname in source1) {
+      if(mergedJSON.hasOwnProperty(attrname)) {
+        if ( source1[attrname]!=null && source1[attrname].constructor==Object ) {
+          mergedJSON[attrname] = mergeJSON(source1[attrname], mergedJSON[attrname]);
+        }
+      } else {
+        mergedJSON[attrname] = source1[attrname];
+      }
+    }
+    return mergedJSON;
+  }
+
+  function generateOptions(givenOptions, defaultOptions) {
+    return mergeJSON(defaultOptions, givenOptions);
+  }
+  function calculateData(data) {
+    if(data instanceof Array) {
+      var sum = data.reduce(function(a, b) {
+        return a + b.data;
+      }, 0);
+      var map = data.map(function(a) {
+        return {
+          label: a.label,
+          data: a.data,
+          color: a.color,
+          portion: a.data/sum
+        }
+      });
+      return map;
+    }
+  }
+})(window)
